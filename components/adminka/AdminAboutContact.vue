82235fdea6d4b36b98e328616268d7edbd3b5c49
Dovran <Orazowd77@gmail.com>
2023-12-04 20:44:16 +0500

    conflickt cheking

--- components/adminka/AdminAboutContact.vue
+++ components/adminka/AdminAboutContact.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class='admin-about-contact'>
+  <div class="admin-about-contact">
     <base-languages @selectLanguage="toggleLanguage" :activeLang="activeLang" />
+    <form class="admin-form" @submit.prevent>
+      <admin-input
+        @updateValue="(val) => (main.phone = val)"
+        :value="main?.phone"
+        label="Phone"
+        class="mb-2"
+        type="number"
+        placeholder="..."
+      />
+    </form>
   </div>
 </template>
 
@@ -8,4 +18,15 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.admin-header-button {
+  grid-column: 1 / span 2;
+}
+.admin-form {
+  padding: 0 36px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  width: 70%;
+  gap: 30px;
+}
+</style>
--- components/adminka/AdminAboutInfo.vue
+++ components/adminka/AdminAboutInfo.vue
@@ -158,29 +158,5 @@ export default {
   grid-template-columns: 1fr 1fr;
   width: 70%;
   gap: 30px;
-  .text-editor-wrapper {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
-    &__text-editor {
-      display: flex;
-      gap: 6px;
-      padding: 6px;
-      border-radius: 8px;
-      background-color: var(--gray-800);
-    }
-
-    &__text-editor-item {
-      padding: 7px;
-      border-radius: 4px;
-      background-color: var(--primary);
-      color: #fff;
-      cursor: pointer;
-    }
-
-    &__calendar {
-    }
-  }
 }
 </style>
